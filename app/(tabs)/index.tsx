3f218ebe7dc42ab0c3ae628abb726f8ce6a0d57d
anszdev <anszdev@outlook.com>
2025-07-09 23:21:58 -0600

    fix: remove safe area hook and add safe area context component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,24 @@
 import { View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ListAppointment } from "@/components/appointment/ListAppointment";
 import { Calendar } from "@/components/calendar/Calendar";
 import { Header } from "@/components/ui/Header";
 
 export default function Index() {
-  const insets = useSafeAreaInsets();
   return (
-    <View
-      style={{
-        paddingTop: insets.top + 16,
-        paddingBottom: insets.bottom,
-      }}
-    >
-      <Header />
+    <SafeAreaView>
+      <View
+        style={{
+          paddingTop: 16,
+        }}
+      >
+        <Header />
 
-      <Calendar />
+        <Calendar />
 
-      <ListAppointment />
-    </View>
+        <ListAppointment />
+      </View>
+    </SafeAreaView>
   );
 }
