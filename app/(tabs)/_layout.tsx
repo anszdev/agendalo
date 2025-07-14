995f8907b0611dc48cf71742f5faf10a11a041a3
anszdev <anszdev@outlook.com>
2025-07-14 12:27:50 -0600

    Add bottom navigation bar with tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Fontisto } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
@@ -7,14 +7,52 @@ export default function TabLayout() {
       screenOptions={{
         headerShown: false,
         tabBarLabel: "",
+        tabBarStyle: {
+          position: "absolute",
+          borderRadius: 999,
+          bottom: 16,
+          width: "auto",
+          height: 40,
+          backgroundColor: "#fff",
+          padding: 0,
+        },
+        tabBarIconStyle: {
+          backgroundColor: "red",
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
+          tabBarIcon: () => <Feather name="calendar" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="reports"
+        options={{
           tabBarIcon: () => (
-            <Fontisto name="calendar" size={24} color="black" />
+            <Feather name="file-text" size={24} color="black" />
           ),
+        }}
+      />
+      <Tabs.Screen
+        name="appointment"
+        options={{
+          tabBarIcon: () => <Feather name="plus" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="sale"
+        options={{
+          tabBarIcon: () => (
+            <Feather name="dollar-sign" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="clients"
+        options={{
+          tabBarIcon: () => <Feather name="users" size={24} color="black" />,
         }}
       />
     </Tabs>
