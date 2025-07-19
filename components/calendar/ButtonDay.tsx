dc2461f63031916b797581591a9d5ef21705df72
anszdev <anszdev@outlook.com>
2025-07-19 17:34:41 -0600

    Refactor ButtonDay component to use Day type and improve selection logic

--- components/calendar/ButtonDay.tsx
+++ components/calendar/ButtonDay.tsx
@@ -1,61 +1,70 @@
 import { FONT_WEIGHT } from "@/constants/fonts";
-import { Pressable, Text, StyleSheet } from "react-native";
+import { Day } from "@/types/calendar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 interface ButtonDayProps {
-  day: number | string;
-  month: number;
-  daySelected: number | string | null;
-  selectedDay: (day: number | string) => void;
+  day: Day;
+  selected: Day | null;
+  updateSelectedDay: (day: Day | null) => void;
 }
 
 export const ButtonDay = ({
   day,
-  daySelected,
-  month,
-  selectedDay,
+  selected,
+  updateSelectedDay,
 }: ButtonDayProps) => {
+  const isSelected =
+    day?.day === selected?.day &&
+    day?.month === selected?.month &&
+    day?.year === selected?.year;
+
   return (
-    <Pressable
-      key={`${day}-${month}`}
-      style={[
-        {
-          width: 40,
-          height: 40,
-          borderRadius: 9999,
-          justifyContent: "center",
-          alignItems: "center",
-          borderWidth: 1,
-          borderColor: "transparent",
-          marginHorizontal: 5,
-        },
-        day === daySelected && style.daySelected,
-      ]}
-      onPress={() => selectedDay(day)}
-    >
+    <Pressable style={style.day} onPress={() => updateSelectedDay(day)}>
+      <View style={isSelected && style.daySelected} />
       <Text
-        style={{
-          fontSize: 18,
-          fontFamily: FONT_WEIGHT.semibold,
-          textAlign: "center",
-          color: "black",
-        }}
+        style={[
+          style.text_day,
+          {
+            fontFamily: isSelected ? FONT_WEIGHT.bold : FONT_WEIGHT.medium,
+            color: isSelected ? "#000" : "#333",
+          },
+        ]}
       >
-        {day}
+        {day?.day ?? ""}
       </Text>
     </Pressable>
   );
 };
 
 const style = StyleSheet.create({
+  day: {
+    width: 40,
+    height: 40,
+    borderRadius: 9999,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "transparent",
+    marginHorizontal: 5,
+    position: "relative",
+  },
+  text_day: {
+    fontSize: 18,
+    textAlign: "center",
+  },
   daySelected: {
+    position: "absolute",
     backgroundColor: "#fff",
+    width: 46,
+    height: 46,
+    borderRadius: 9999,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 6,
+      height: 4,
     },
     shadowOpacity: 1,
     shadowRadius: 6,
-    elevation: 6,
+    elevation: 4,
   },
 });
