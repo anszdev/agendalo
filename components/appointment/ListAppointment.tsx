5bcfb7daad9442053fc39993bacb5d9506a469c3
anszdev <anszdev@outlook.com>
2025-07-19 21:38:36 -0600

    Refactor ListAppointment component to utilize useCalendar hook and streamline rendering with FlatList

--- components/appointment/ListAppointment.tsx
+++ components/appointment/ListAppointment.tsx
@@ -1,126 +1,59 @@
 import { FONT_WEIGHT } from "@/constants/fonts";
-import { Feather, Fontisto } from "@expo/vector-icons";
-import { Image, ScrollView, Text, View } from "react-native";
+import { useCalendar } from "@/hooks/calendar/useCalendar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { Appointment } from "./Appointment";
 import { AppointmentHours } from "./AppointmentHours";
 
 export const ListAppointment = () => {
+  const { formattedDayText } = useCalendar();
+
   return (
-    <View
-      style={{
-        borderTopLeftRadius: 32,
-        borderTopRightRadius: 32,
-        paddingHorizontal: 16,
-        paddingVertical: 24,
-        zIndex: 2,
-        position: "relative",
-        backgroundColor: "#fff",
-        marginTop: -24,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 16,
-        }}
-      >
-        <Text style={{ fontSize: 24, fontFamily: FONT_WEIGHT.bold }}>
-          Martes 2
-        </Text>
-        <Text style={{ fontSize: 18, fontFamily: FONT_WEIGHT.semibold }}>
-          3 citas
-        </Text>
+    <View style={st.list_appointment}>
+      <View style={st.list_appointment_header}>
+        <Text style={st.list_appointment_day}>{formattedDayText}</Text>
+        <Text style={st.list_appointment_count}>3 citas</Text>
       </View>
-      <ScrollView>
-        <AppointmentHours hour="11:00" amPm="AM">
-          <Appointment name="Tulio Treviño" time="11:00" />
-          <Appointment name="Tulio Treviño" time="11:00" />
-          <Appointment name="Tulio Treviño" time="11:00" />
-          <Appointment name="Tulio Treviño" time="11:00" />
-        </AppointmentHours>
-        <View style={{ flexDirection: "row", gap: 16 }}>
-          <View style={{ width: "auto" }}>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>03:00</Text>
-            <Text style={{ fontSize: 14, alignSelf: "flex-end" }}>PM</Text>
-          </View>
-          <View
-            style={{
-              borderTopWidth: 1,
-              flex: 1,
-              marginTop: 6,
-              borderColor: "#ccc",
-              paddingTop: 16,
-              paddingBottom: 24,
-              gap: 12,
-            }}
-          >
-            <View
-              style={{
-                padding: 6,
-                borderRadius: 9999,
-                backgroundColor: "#FBCEB1",
-                flexDirection: "row",
-                gap: 12,
-                alignItems: "center",
-              }}
-            >
-              <Image
-                source={require("@assets/images/avatar.jpg")}
-                style={{ width: 70, height: 70, borderRadius: 9999 }}
-              />
-              <View>
-                <Text style={{ fontSize: 24, fontWeight: "bold" }}>Juanin</Text>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    gap: 4,
-                    alignItems: "center",
-                  }}
-                >
-                  <Feather name="clock" size={16} color="black" />
-                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-                    03:00
-                  </Text>
-                </View>
-              </View>
-            </View>
-            <View
-              style={{
-                padding: 6,
-                borderRadius: 9999,
-                backgroundColor: "#FFFFCC",
-                flexDirection: "row",
-                gap: 12,
-                alignItems: "center",
-              }}
-            >
-              <Image
-                source={require("@assets/images/avatar.jpg")}
-                style={{ width: 70, height: 70, borderRadius: 9999 }}
-              />
-              <View>
-                <Text style={{ fontSize: 24, fontWeight: "bold" }}>
-                  Don Pool
-                </Text>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    gap: 4,
-                    alignItems: "center",
-                  }}
-                >
-                  <Fontisto name="clock" size={16} color="black" />
-                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-                    03:45
-                  </Text>
-                </View>
-              </View>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
+      <View>
+        <FlatList
+          data={[1, 2, 3, 4, 5]}
+          keyExtractor={(item) => item.toString()}
+          renderItem={({ item }) => (
+            <AppointmentHours hour="11:00" amPm="AM">
+              <Appointment name="Tulio Treviño" time="11:00" />
+            </AppointmentHours>
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 16 }}
+        />
+      </View>
     </View>
   );
 };
+
+const st = StyleSheet.create({
+  list_appointment: {
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    zIndex: 2,
+    position: "relative",
+    backgroundColor: "#fff",
+    marginTop: -24,
+  },
+  list_appointment_header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  list_appointment_day: {
+    fontSize: 24,
+    fontFamily: FONT_WEIGHT.bold,
+    textTransform: "capitalize",
+  },
+  list_appointment_count: {
+    fontSize: 18,
+    fontFamily: FONT_WEIGHT.semibold,
+  },
+});
