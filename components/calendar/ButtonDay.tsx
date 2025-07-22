eb72fcdcf6f20ddfe781c85cad85a4e45b8401a9
anszdev <anszdev@outlook.com>
2025-07-22 10:33:08 -0600

    Enhance ButtonDay component with animated selection and active indicator

--- components/calendar/ButtonDay.tsx
+++ components/calendar/ButtonDay.tsx
@@ -1,26 +1,50 @@
+import { COLORS } from "@/constants/colors";
 import { FONT_WEIGHT } from "@/constants/fonts";
 import { Day } from "@/types/calendar";
+import { useEffect } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface ButtonDayProps {
   day: Day;
   selected: Day | null;
   updateSelectedDay: (day: Day | null) => void;
+  activeIndicator?: boolean;
 }
 
 export const ButtonDay = ({
   day,
   selected,
   updateSelectedDay,
+  activeIndicator = false,
 }: ButtonDayProps) => {
   const isSelected =
     day?.day === selected?.day &&
     day?.month === selected?.month &&
     day?.year === selected?.year;
 
+  const animatedOpacity = useSharedValue(isSelected ? 1 : 0);
+
+  useEffect(() => {
+    animatedOpacity.value = withTiming(isSelected ? 1 : 0);
+  }, [isSelected, animatedOpacity]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: animatedOpacity.value,
+  }));
+
   return (
-    <Pressable style={style.day} onPress={() => updateSelectedDay(day)}>
-      <View style={isSelected && style.daySelected} />
+    <Pressable
+      style={style.day}
+      onPress={() => updateSelectedDay(day)}
+      onLongPress={() => console.log("Long Pressed", day)}
+    >
+      <Animated.View style={[isSelected && style.daySelected, animatedStyle]} />
+
       <Text
         style={[
           style.text_day,
@@ -32,6 +56,7 @@ export const ButtonDay = ({
       >
         {day?.day ?? ""}
       </Text>
+      {activeIndicator && day.day !== null && <View style={style.indicator} />}
     </Pressable>
   );
 };
@@ -55,8 +80,8 @@ const style = StyleSheet.create({
   daySelected: {
     position: "absolute",
     backgroundColor: "#fff",
-    width: 46,
-    height: 46,
+    width: 44,
+    height: 44,
     borderRadius: 9999,
     shadowColor: "#000",
     shadowOffset: {
@@ -66,5 +91,15 @@ const style = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 6,
     elevation: 4,
+  },
+  indicator: {
+    position: "absolute",
+    bottom: 2,
+    left: "50%",
+    width: 5,
+    height: 5,
+    backgroundColor: COLORS.a_primary,
+    borderRadius: 999,
+    transform: [{ translateX: -2.5 }],
   },
 });
