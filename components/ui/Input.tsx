0499967299bcea263fe0e80a04e0426346f8edf0
Angel Salazar <anszdev@outlook.com>
2025-08-05 23:46:54 -0600

    refactor: streamline Input component structure and styles for improved readability

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,47 +1,60 @@
 import { COLORS } from "@/constants/colors";
 import { FONT_WEIGHT } from "@/constants/fonts";
-import { Text, TextInput, View } from "react-native";
+import {
+  InputModeOptions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 interface InputProps {
   placeholder?: string;
   value?: string;
   onChangeText: (text: string) => void;
-  fontSize?: number;
   label: string;
+  inputMode?: InputModeOptions;
 }
 
 export const Input = ({
-  fontSize = 22,
   onChangeText,
   placeholder,
   value,
   label,
+  inputMode = "text",
 }: InputProps) => {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text
-        style={{
-          fontSize: 18,
-          fontFamily: FONT_WEIGHT.semibold,
-          color: COLORS.a_textPrimary,
-        }}
-      >
-        {label}
-      </Text>
+      <Text style={styles.label}>{label}</Text>
       <TextInput
-        style={{
-          fontSize: fontSize,
-          fontFamily: FONT_WEIGHT.bold,
-          textTransform: "capitalize",
-          padding: 0,
-        }}
+        style={styles.input}
         placeholder={placeholder}
         placeholderTextColor="#eee"
         cursorColor={COLORS.a_textPrimary}
         underlineColorAndroid="transparent"
         onChangeText={onChangeText}
         value={value}
+        inputMode={inputMode}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 16,
+    fontFamily: FONT_WEIGHT.semibold,
+    color: COLORS.a_textPrimary,
+  },
+  input: {
+    fontSize: 22,
+    fontFamily: FONT_WEIGHT.bold,
+    textTransform: "capitalize",
+    padding: 0,
+    paddingBottom: 4,
+    width: "auto",
+    borderBottomWidth: 2,
+    borderBottomColor: COLORS.a_textPrimary,
+    color: "#000",
+  },
+});
